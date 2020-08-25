7db50cb76cd38063feb96d015ae0f533d85bf0f5
Matheus Sousa <ma.sousa.soares@gmail.com>
2020-08-24 22:30:33 -0300

    Versão do prof.

--- Program.cs
+++ Program.cs
@@ -7,33 +7,36 @@ namespace _1048
     {
         static void Main(string[] args)
         {
-            double salario, novoSalario, ganho;
-            int percentual;
+            
+            
 
-            salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                             
-            if (salario ==0 || salario < 400.00){
-                novoSalario = salario * 1.15;
-                ganho = novoSalario - salario;
-                percentual = 15;
+            if (salario <= 400.0){   
                 
-            } else if (salario >= 400.01 && salario <= 800.00){
-                novoSalario = salario * 1.12;
-                ganho = novoSalario - salario;
-                percentual = 12;
-            } else if (salario >= 800.01 && salario <= 1200.00){
-                novoSalario = salario * 1.10;
-                ganho = novoSalario - salario;
-                percentual = 10;
-            } else  {
-                novoSalario = salario * 1.07;
-                ganho = novoSalario - salario;
-                percentual = 7;
-            }   
+                percentual = 15.0;
+                
+            } else if (salario <= 800.0){
+                
+                percentual = 12.0;
+            } else if (salario <= 1200.0){
+               
+                percentual = 10.0;
+            } else if (salario <= 2000.0) {
+                
+                percentual = 7.0;
+            }   else {
+                
+                percentual = 4.0;
+            }
+
+           double ganho = salario * percentual / 100.0;
+           double novoSalario = salario + ganho;
+    
 
             Console.WriteLine("Novo salario: "+ novoSalario.Tostring("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Reajuste ganho: "+ ganho.Tostring("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Em percentual: " + percentual + " %");
+            Console.WriteLine("Em percentual: " + percentual.Tostring("F0", CultureInfo.InvariantCulture) + " %");
         }
     }
 }
