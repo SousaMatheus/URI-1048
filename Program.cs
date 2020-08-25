a947cefbb9596ac0c24739543156b4c2571c77bc
Matheus Sousa <ma.sousa.soares@gmail.com>
2020-08-24 22:37:23 -0300

    arrumei declaracao varial percentual

--- Program.cs
+++ Program.cs
@@ -5,38 +5,34 @@ namespace _1048
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            
-            
+          static void Main(string[] args) {
 
             double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                            
-            if (salario <= 400.0){   
-                
+
+            double percentual;
+            
+            if (salario <= 400.0) {
                 percentual = 15.0;
-                
-            } else if (salario <= 800.0){
-                
+            }
+            else if (salario <= 800.0) {
                 percentual = 12.0;
-            } else if (salario <= 1200.0){
-               
+            }
+            else if (salario <= 1200.0) {
                 percentual = 10.0;
-            } else if (salario <= 2000.0) {
-                
+            }
+            else if (salario <= 2000.0) {
                 percentual = 7.0;
-            }   else {
-                
+            }
+            else {
                 percentual = 4.0;
             }
 
-           double ganho = salario * percentual / 100.0;
-           double novoSalario = salario + ganho;
-    
+            double reajuste = salario * percentual / 100.0;
+            double novoSalario = salario + reajuste;
 
-            Console.WriteLine("Novo salario: "+ novoSalario.Tostring("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Reajuste ganho: "+ ganho.Tostring("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Em percentual: " + percentual.Tostring("F0", CultureInfo.InvariantCulture) + " %");
+            Console.WriteLine("Novo salario: " + novoSalario.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Reajuste ganho: " + reajuste.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Em percentual: " + percentual.ToString("F0", CultureInfo.InvariantCulture) + " %");
         }
     }
 }
